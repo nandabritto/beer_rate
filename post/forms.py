437baecf2a99038bfc3457e20625902668e2d5e6
nandabritto <nanda.britto@gmail.com>
2021-10-17 14:17:34 +0000

    Add base template

--- post/forms.py
+++ post/forms.py
@@ -6,7 +6,3 @@ class Beer_Review_Form(forms.ModelForm):
     class Meta:
         model = BeerReview
         fields = ['beer_style', 'beer', 'user_name', 'review','bitterness', 'money_value']
-
-# form = BeerReview()
-# beer_review = BeerReview.objects.get(pk=1)
-# form = Beer_Review_Form(instance=beer_review)
