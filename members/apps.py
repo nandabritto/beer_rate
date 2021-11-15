3303b3208ab291ab070d9e63cdf5030ffc04475e
nandabritto <nanda.britto@gmail.com>
2021-11-15 21:57:48 +0000

    Add test to register invalid

--- members/apps.py
+++ members/apps.py
@@ -1,6 +1,8 @@
+''' System Module '''
 from django.apps import AppConfig
 
 
 class MembersConfig(AppConfig):
+    ''' Members App Confuiguration '''
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'members'
--- members/test_views.py
+++ members/test_views.py
@@ -1,45 +1,15 @@
+'''System Module '''
+
 from django.test import TestCase
 from django.contrib.auth.models import User
-from django.shortcuts import redirect 
 from django.urls import reverse
 
 
+class TestRegister(TestCase):
+    ''' Test Register Function '''
 
-class test_login(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(
-            username='fernanda', email='fernanda@gmail.com', password='12345')
-
-    def test_user_can_login(self):
-        response = self.client.post(reverse('login'), {"username": "fernanda", "password": "12345"})
-        self.assertRedirects(response, '/', status_code=302, 
-        target_status_code=200, fetch_redirect_response=True)
-           
-    def test_user_cannot_login(self):
-        response = self.client.post(reverse('login'), {"username": "", "password": "12345"})
-        self.assertRedirects(response, '/members/login_user', status_code=302, 
-        target_status_code=200, fetch_redirect_response=True)
-        
-    # def test_user_is_not_None(self):
-    #     response = self.client.post(reverse('login'), {"username": "", "password": "12345"})
-    #     # if self.user.username is None:
-    #     #     raise ValidationError("You must have a username")
-
-
-class test_logout(TestCase):
-
-    def setUp(self):
-        self.user = User.objects.create_user(
-            username='fernanda', email='fernanda@gmail.com', password='12345')
-
-    def test_logout(self):
-        response = self.client.get(reverse('logout'))
-        self.assertRedirects(response, '/', status_code=302)
-
-
-class test_register(TestCase):
-
-    def test_register__user(self):
+    def test_register_user(self):
+        ''' Teste redirection when user register '''
         response = self.client.post(reverse('register'), data={
             'username': 'FernandaB',
             'email': 'fernandab@gmail.com',
@@ -48,3 +18,67 @@ class test_register(TestCase):
         })
         self.assertEqual(response.status_code, 302)
 
+    def test_register_user_invalid(self):
+        response = self.client.post(reverse('register'), data={
+            'username': 'FernandaB',
+            'email': 'fernandab@gmail.com',
+            'password1': '1234567',
+            'password2': '123456'
+        })
+        self.assertRedirects(
+            response, '/members/register_user',
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True)
+
+
+
+class TestLogin(TestCase):
+    ''' Test Log In Function '''
+    def setUp(self):
+        ''' Setup to test login '''
+        self.user = User.objects.create_user(
+            username='fernanda',
+            email='fernanda@gmail.com',
+            password='12345')
+
+    def test_user_can_login(self):
+        ''' Test redirection if user log in '''
+        response = self.client.post(reverse(
+            'login'), {
+            "username": "fernanda",
+            "password": "12345"})
+        self.assertRedirects(
+            response, '/',
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True)
+
+    def test_user_cannot_login(self):
+        ''' Test redirection if user cannot log in '''
+
+        response = self.client.post(reverse(
+            'login'), {
+            "username": "",
+            "password": "12345"})
+        self.assertRedirects(
+            response,
+            '/members/login_user',
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True)
+
+
+class TestLogout(TestCase):
+    ''' Test Log out Function '''
+    def setUp(self):
+        ''' Setup to test logout '''
+        self.user = User.objects.create_user(
+            username='fernanda',
+            email='fernanda@gmail.com',
+            password='12345')
+
+    def test_logout(self):
+        ''' Test redirection if user logout '''
+        response = self.client.get(reverse('logout'))
+        self.assertRedirects(response, '/', status_code=302)
