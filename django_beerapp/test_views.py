ca24e66fd8ae8a63f2adbdf8034d365994630340
nandabritto <nanda.britto@gmail.com>
2021-11-27 15:00:31 +0000

    Add unittest for error 404 function

--- django_beerapp/test_views.py
+++ django_beerapp/test_views.py
@@ -9,3 +9,9 @@ class TestHttpErrors(TestCase):
         response = self.client.get(
             '/review_list/review_detail/0')
         self.assertEqual(response.status_code, 404)
+
+    def test_page_not_found_view(self):
+        """ Test if review detail url exist """
+        response = self.client.get(
+            '/review_list/review_detail/0')
+        self.assertEqual(response.status_code, 403)
